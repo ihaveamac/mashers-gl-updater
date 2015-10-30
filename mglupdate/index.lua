146e60d05c3c9bf6e810c25b7f94dc147b2f2018
ihaveamac <ihaveamac@icloud.com>
2015-10-30 07:02:35 -0700

    hide version numbr unless R is held. add delay before doing networking things to see if that solves #1

--- mglupdate/index.lua
+++ mglupdate/index.lua
@@ -4,13 +4,13 @@ getstatus_url = "http://ianburgwin.net/mglupdate/getstate.php"
 boot3dsx_url = "http://ianburgwin.net/mglupdate/boot1.3dsx"
 -- as in README.md, https sites don't work in ctrulib, unless there's a workaround
 
-function updateState(stype, info)
+function updateState(stype, info, info2)
 	Screen.refresh()
 	Screen.clear(TOP_SCREEN)
-	Screen.debugPrint(5, 5, "mashers's Grid Launcher Update v1.06", Color.new(255, 255, 255), TOP_SCREEN)
+	Screen.debugPrint(5, 5, "mashers's Grid Launcher Update v1.1", Color.new(255, 255, 255), TOP_SCREEN)
 	Screen.fillEmptyRect(0,399,17,18,Color.new(255, 255, 255), TOP_SCREEN)
 	if stype == "gettingver" then
-		Screen.debugPrint(5, 25, "Getting latest version number", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 25, "Preparing", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.flip()
 	elseif stype == "gettingver" then
 		Screen.debugPrint(5, 25, "The server is busy - waiting", Color.new(255, 255, 255), TOP_SCREEN)
@@ -29,7 +29,18 @@ function updateState(stype, info)
 			end
 		end
 	elseif stype == "showversion" then
-		Screen.debugPrint(5, 25, "The latest beta is "..info.."", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 25, "(DEBUG) The latest beta is'"..info.."'", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 45, "Press A to download & update", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 60, "Press B to exit", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 80, "Full result: "..info2, Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.flip()
+		while true do
+			local pad = Controls.read()
+			if Controls.check(pad, KEY_B) then exit()
+			elseif Controls.check(pad, KEY_A) then return end
+		end
+	elseif stype == "errorversion" then
+		Screen.debugPrint(5, 25, "Do you want to update?", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.debugPrint(5, 45, "Press A to download & update", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.debugPrint(5, 60, "Press B to exit", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.flip()
@@ -39,7 +50,7 @@ function updateState(stype, info)
 			elseif Controls.check(pad, KEY_A) then return end
 		end
 	elseif stype == "downloading" then
-		Screen.debugPrint(5, 25, "Downloading beta "..info..", sit tight", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 25, "Downloading version"..info..", sit tight", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.flip()
 	elseif stype == "done" then
 		Screen.debugPrint(5, 25, "Downloaded & installed!", Color.new(255, 255, 255), TOP_SCREEN)
@@ -62,7 +73,14 @@ function exit()
 end
 
 -- READY:#define currentversion XX
--- string.sub 30                ^
+-- string.sub 29               ^
+-- this includes the space. this is intended.
+
+-- seeing if this gives the networking stuff time to start up
+ti = Timer.new()
+Timer.resume(ti)
+while Timer.getTime(ti) <= 250 do end
+Timer.destroy(ti)
 
 Screen.waitVblankStart()
 updateState("gettingver")
@@ -74,7 +92,7 @@ function getServerState()
 		-- Network.requestString did not seem to work properly.
 		Network.downloadFile(getstatus_url, System.currentDirectory().."/tmp/state")
 		local tmp_s = io.open(System.currentDirectory().."/tmp/state", FREAD)
-		state = io.read(tmp_s, 0, io.size(tmp_s))
+		fullstate = io.read(tmp_s, 0, io.size(tmp_s))
 		io.close(tmp_s)
 	end)
 	if not status then
@@ -82,6 +100,7 @@ function getServerState()
 	end
 end
 getServerState()
+
 if state == "DOWNLOADING" then
 	-- expects the boot.3dsx to be cached on the server quickly. normally won't take more than 1-2 seconds
 	updateState("cacheupdating")
@@ -91,8 +110,12 @@ if state == "DOWNLOADING" then
 	Timer.destroy(ti)
 	getServerState()
 end
-state = string.sub(state, 30)
-updateState("showversion", state)
+sstate = string.sub(fullstate, 29)
+if Controls.check(Controls.read(), KEY_R) then
+	updateState("showversion", sstate, fullstate)
+else
+	updateState("errorversion")
+end
 updateState("downloading", state)
 Network.downloadFile(boot3dsx_url, System.currentDirectory().."/tmp/boot1.3dsx")
 System.deleteFile(boot3dsx_location)
