4cd6599272430a41f7711ba84ab2fd8fa8194c14
ihaveamac <ihaveamac@icloud.com>
2015-10-30 12:33:58 -0700

    rename index.php -> .html. start caching index.lua for updater.

--- site/index.php
+++
@@ -1,2 +0,0 @@
-<p>This part of the site is for the updater for masher's grid launcher. https://gbatemp.net/threads/release-homebrew-launcher-with-grid-layout.397527/</p>
-<p>I don't think you're normally supposed to be here...</p>
