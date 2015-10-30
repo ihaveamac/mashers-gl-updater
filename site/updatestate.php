3ce7439fa8648fdd3d1dd610b56f4fbd17e0b03c
ihaveamac <ihaveamac@icloud.com>
2015-10-30 13:34:53 -0700

    add separate version file for the updater

--- site/updatestate.php
+++ site/updatestate.php
@@ -6,17 +6,20 @@ if ($versionh_local == "notready") {
 } else {
 	$versionh_server = file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/source/version.h");
 	if (strcmp($versionh_server, $versionh_local) !== 0) {
-		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h", "notready"); // notready = server is busy to client
+		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version_info", "notready"); // notready = server is busy to client
 		
 		$boot3dsx = file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/boot1.3dsx");
 		
 		unlink($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h");
+		unlink($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version_info");
 		unlink($_SERVER["DOCUMENT_ROOT"]."/mglupdate/boot1.3dsx");
+		$version_info = substr($versionh_server, 23)."|".file_get_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/mglu_version");
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h", $versionh_server);
+		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version_info", $version_info);
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/boot1.3dsx", $boot3dsx);
-		$result = "READY:".$versionh_server;
+		$result = "READY";
 	} else {
-		$result = "READY:".$versionh_local;
+		$result = "READY";
 	}
 }
 echo $result;
