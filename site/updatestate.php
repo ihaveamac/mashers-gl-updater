1a7357b1c2b781729e0d5bf52b23e23e2fa36024
ihaveamac <ihaveamac@icloud.com>
2015-10-30 13:06:03 -0700

    remove state file, no longer needed

--- site/updatestate.php
+++ site/updatestate.php
@@ -1,12 +1,11 @@
 <?php
 $result = ".";
-if (file_get_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/state.txt") == "DOWNLOADING") {
+$versionh_local = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h");
+if ($versionh_local == "notready") {
 	$result = "DOWNLOADING";
 } else {
-	$versionh_local = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h");
 	$versionh_server = file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/source/version.h");
 	if (strcmp($versionh_server, $versionh_local) !== 0) {
-		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/state.txt", "DOWNLOADING");
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h", "notready"); // notready = server is busy to client
 		
 		$boot3dsx = file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/boot1.3dsx");
@@ -15,7 +14,6 @@ if (file_get_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/state.txt") == "DOWN
 		unlink($_SERVER["DOCUMENT_ROOT"]."/mglupdate/boot1.3dsx");
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/version.h", $versionh_server);
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/boot1.3dsx", $boot3dsx);
-		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate/state.txt", "READY");
 		$result = "READY:".$versionh_server;
 	} else {
 		$result = "READY:".$versionh_local;
