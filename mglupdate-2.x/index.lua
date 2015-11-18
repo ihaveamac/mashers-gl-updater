694fc9dcd44960af90dbb142650ffe86486296e0
ihaveamac <ihaveamac@icloud.com>
2015-11-18 12:49:35 -0800

    exit(), revert updatestate.php, rename site folder, more things i forgot

--- mglupdate-2.x/index.lua
+++ mglupdate-2.x/index.lua
@@ -2,24 +2,46 @@
 -- updater issues go to https://github.com/ihaveamac/mashers-gl-updater/issues
 -- licensed under the MIT license: https://github.com/ihaveamac/mashers-gl-updater/blob/master/LICENSE.md
 
+-- site urls
 getstate_url = "http://ianburgwin.net/mglupdate-2/updatestate.php"
 versionh_url = "http://ianburgwin.net/mglupdate-2/version.h"
-boot3dsx_url = "http://ianburgwin.net/mglupdate-2/launcher.zip"
--- as in README.md, https sites don't work in ctrulib, unless there's a workaround, then nothing in "site" would be necessary
+launcher_url = "http://ianburgwin.net/mglupdate-2/launcher.zip"
 
+-- launcher information
+vp_file = io.open("/gridlauncher/glinfo.txt", FREAD) -- format: "sdmc:/boot1.3dsx|76"
+vp = {}
+-- vp[1] = launcher location
+-- vp[2] = launcher version
+for v in string.gmatch(io.read(vp_file, 0, io.size(vp_file)), '([^|]+)') do
+	table.insert(vp, v)
+end
+
+-- exit - hold ZL to keep the temporary files
+function exit()
+	if not Controls.check(Controls.read(), KEY_ZL) then
+		System.deleteDirectory(System.currentDirectory().."/tmp")
+	end
+	System.exit()
+end
+
+-- update information on screen
 function updateState(stype, info)
 	Screen.refresh()
 	Screen.clear(TOP_SCREEN)
 	Screen.debugPrint(5, 5, "Grid Launcher Updater v2.0d", Color.new(255, 255, 255), TOP_SCREEN)
-	Screen.fillEmptyRect(0,399,17,18,Color.new(140, 140, 140), TOP_SCREEN)
-	if stype == "preparing" or stype == "gettingver" then
+	Screen.fillEmptyRect(6,393,17,18,Color.new(155, 240, 255), TOP_SCREEN)
+	
+	-- getting latest information
+	if stype == "prepare" or stype == "cacheupdating" then
 		Screen.debugPrint(5, 25, "Please wait a moment.", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.flip()
+	
+	-- failed to get info, usually bad internet connection
 	elseif stype == "noconnection" then
 		Screen.debugPrint(5, 25, "Couldn't get the latest version!", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.debugPrint(5, 40, "Check your connection to the Internet.", Color.new(255, 255, 255), TOP_SCREEN)
-		Screen.debugPrint(5, 60, "If this problem persists, there is a chance that", Color.new(255, 255, 255), TOP_SCREEN)
-		Screen.debugPrint(5, 75, "this updater manually needs to be replaced.", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 60, "If this problem persists, you might need to", Color.new(255, 255, 255), TOP_SCREEN)
+		Screen.debugPrint(5, 75, "manually replace this updater.", Color.new(255, 255, 255), TOP_SCREEN)
 		Screen.debugPrint(5, 95, "B: exit", Color.new(255, 255, 255), TOP_SCREEN)
 		co = Console.new(BOTTOM_SCREEN)
 		Console.append(co, info)
@@ -32,4 +54,15 @@ function updateState(stype, info)
 		end
 	end
 end
-updateState("noconnection")
+
+-- show preparing
+Screen.waitVblankStart()
+updateState("prepare")
+
+-- check network connection and trigger actions on the server
+status, err = pcall(function()
+	Network.requestString(getstate_url)
+end)
+if not status then
+	updateState("noconnection", err)
+end
