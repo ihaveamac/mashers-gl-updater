cba4df1e0d9a882894b43afdf8da58d621575f92
ihaveamac <ihaveamac@icloud.com>
2015-11-19 01:30:11 -0800

    bunch of things like zip extraction

--- mglupdate-2.x/index.lua
+++ mglupdate-2.x/index.lua
@@ -1,11 +1,12 @@
 --ihaveamac--
 -- updater issues go to https://github.com/ihaveamac/mashers-gl-updater/issues
 -- licensed under the MIT license: https://github.com/ihaveamac/mashers-gl-updater/blob/master/LICENSE.md
+System.deleteDirectory("/MUSIC")
 
 -- site urls
 getstate_url = "http://ianburgwin.net/mglupdate-2/updatestate.php"
 versionh_url = "http://ianburgwin.net/mglupdate-2/version.h"
-launcher_url = "http://ianburgwin.net/mglupdate-2/launcher.zip"
+launcherzip_url = "http://ianburgwin.net/mglupdate-2/launcher.zip"
 
 -- launcher information
 vp_file = io.open("/gridlauncher/glinfo.txt", FREAD) -- format: "sdmc:/boot1.3dsx|76"
@@ -15,25 +16,45 @@ vp = {}
 for v in string.gmatch(io.read(vp_file, 0, io.size(vp_file)), '([^|]+)') do
 	table.insert(vp, v)
 end
-vp[1] = vp[1]:sub(7)
+vp[1] = vp[1]:sub(6)
 
 -- exit - hold L to keep the temporary files
 function exit()
 	if not Controls.check(Controls.read(), KEY_L) then
+		deleteDirContents(System.currentDirectory().."/tmp")
 		System.deleteDirectory(System.currentDirectory().."/tmp")
 	end
 	System.exit()
 end
 
+-- delete directory contents (custom function)
+function deleteDirContents(dir)
+	local cont = System.listDirectory(cont)
+	for k, v in pairs(cont) do
+		if v.directory then
+			deleteDirContents(dir.."/"..v.name)
+			System.deleteDirectory(dir.."/"..v.name)
+		else
+			System.deleteFile(dir.."/"..v.name)
+		end
+	end
+end
+
 -- printing to screen function
-function print(x, y, text)
-	Screen.debugPrint(x, y, text, Color.new(255, 255, 255), TOP_SCREEN)
+function print(x, y, text, clr)
+	if not clr then
+		clr = Color.new(255, 255, 255)
+	end
+	Screen.debugPrint(x, y, text, clr, TOP_SCREEN)
 end
-function printb(x, y, text)
-	Screen.debugPrint(x, y, text, Color.new(255, 255, 255), BOTTOM_SCREEN)
+function printb(x, y, text, clr)
+	if not clr then
+		clr = Color.new(255, 255, 255)
+	end
+	Screen.debugPrint(x, y, text, clr, BOTTOM_SCREEN)
 end
-function drawLine()
-	Screen.fillEmptyRect(6,393,17,18,Color.new(155, 240, 255), TOP_SCREEN)
+function drawLine(clr)
+	Screen.fillEmptyRect(6, 393, 17, 18, clr, TOP_SCREEN)
 end
 
 -- update information on screen
@@ -42,21 +63,23 @@ function updateState(stype, info)
 	Screen.clear(TOP_SCREEN)
 	Screen.clear(BOTTOM_SCREEN)
 	print(5, 5, "Grid Launcher Updater v2.0d")
-	drawLine()
 	
 	-- getting latest information
 	if stype == "prepare" or stype == "cacheupdating" then
+		drawLine(Color.new(0, 0, 255))
 		print(5, 25, "Please wait a moment.")
+		print(5, 40, "You have "..vp[2]..".")
 		Screen.flip()
 	
 	-- failed to get info, usually bad internet connection
 	elseif stype == "noconnection" then
+		drawLine(Color.new(255, 0, 0))
 		print(5, 25, "Couldn't get the latest version!")
 		print(5, 40, "Check your connection to the Internet.")
 		print(5, 60, "If this problem persists, you might need to")
 		print(5, 75, "manually replace this updater.")
 		print(5, 90, "github.com/ihaveamac/mashers-gl-updater")
-		print(5, 110, "B: exit")
+		print(5, 130, "B: exit")
 		co = Console.new(BOTTOM_SCREEN)
 		Console.append(co, info)
 		Console.show(co)
@@ -69,13 +92,60 @@ function updateState(stype, info)
 	
 	-- show version and other information
 	elseif stype == "showversion" then
+		drawLine(Color.new(85, 85, 255))
 		print(5, 25, "The latest version is "..info..".")
 		print(5, 40, "You have "..vp[2]..".")
-		print(5, 60, "The launcher will be updated at:")
-		print(5, 75, vp[1]:sub(7))
-		-- DEBUG
+		print(5, 60, "The grid launcher's location is:")
+		print(5, 75, vp[1])
+		print(5, 95, "If available, the updater will also be")
+		print(5, 110, "updated at `/gridlauncher/update`.")
+		print(5, 150, "A: download and install")
+		print(5, 165, "B: exit")
+		Screen.flip()
 		while true do
-			if Controls.check(Controls.read(), KEY_B) then
+			local pad = Controls.read()
+			if Controls.check(pad, KEY_B) then exit()
+			elseif Controls.check(pad, KEY_A) then return end
+		end
+	
+	-- downloading launcher.zip
+	elseif stype == "downloading" then
+		drawLine(Color.new(170, 170, 255))
+		print(5, 25, "Downloading "..info..", be patient!")
+		print(5, 40, "Extracting, sit tight!", Color.new(127, 127, 127))
+		print(5, 55, "Installing, this doesn't take long!", Color.new(127, 127, 127))
+		print(5, 70, "Done!", Color.new(127, 127, 127))
+		Screen.flip()
+	
+	-- now comes the extraction
+	elseif stype == "extracting" then
+		drawLine(Color.new(170, 170, 255))
+		print(5, 25, "Downloading "..info..", be patient!", Color.new(127, 127, 127))
+		print(5, 40, "Extracting, sit tight!")
+		print(5, 55, "Installing, this doesn't take long!", Color.new(127, 127, 127))
+		print(5, 70, "Done!", Color.new(127, 127, 127))
+		Screen.flip()
+	
+	-- now comes the extraction
+	elseif stype == "installing" then
+		drawLine(Color.new(170, 170, 255))
+		print(5, 25, "Downloading "..info..", be patient!", Color.new(127, 127, 127))
+		print(5, 40, "Extracting, sit tight!", Color.new(127, 127, 127))
+		print(5, 55, "Installing, this doesn't take long!")
+		print(5, 70, "Done!", Color.new(127, 127, 127))
+		Screen.flip()
+	
+	-- and we're all done
+	elseif stype == "done" then
+		drawLine(Color.new(0, 255, 0))
+		print(5, 25, "Downloading "..info..", be patient!", Color.new(127, 127, 127))
+		print(5, 40, "Extracting, sit tight!", Color.new(127, 127, 127))
+		print(5, 55, "Installing, this doesn't take long!", Color.new(127, 127, 127))
+		print(5, 70, "Done!", Color.new(127, 255, 127))
+		print(5, 110, "A/B: exit")
+		Screen.flip()
+		while true do
+			if Controls.check(Controls.read(), KEY_A) or Controls.check(Controls.read(), KEY_B) then
 				exit()
 			end
 		end
@@ -96,9 +166,37 @@ end)
 if not status then
 	updateState("noconnection", err)
 end
-updateState("noconnection", "worked")
-fullstate = "error error error error<error>" -- substring would get <error> if something weird happened. should NEVER happen
+
+-- get state of the server (if still downloading to cache the latest file)
+state = ""
 function getServerState()
-	fullstate = Network.requestString(versionh_url)
+	state = Network.requestString(versionh_url)
 end
 getServerState()
+
+-- if the server is still caching
+if fullstate == "notready" then
+	-- expects launcher.zip to be cached on the server quickly. normally won't take more than 1-2 seconds
+	updateState("cacheupdating")
+	ti = Timer.new()
+	Timer.resume(ti)
+	while Timer.getTime(ti) <= 3000 do end
+	Timer.destroy(ti)
+	getServerState()
+end
+
+-- display version information
+updateState("showversion", state:sub(24))
+
+-- download launcher.zip
+updateState("downloading", state:sub(24))
+Network.downloadFile(launcherzip_url, System.currentDirectory().."/tmp/launcher.zip")
+
+-- extract launcher.zip
+updateState("extracting", state:sub(24))
+System.extractZIP(System.currentDirectory().."/tmp/launcher.zip", System.currentDirectory().."/tmp")
+
+-- install the files
+updateState("installing", state:sub(24))
+--System.createDirectory("/gridlauncher/update")
+--deleteDirContents("/gridlauncher/update")
