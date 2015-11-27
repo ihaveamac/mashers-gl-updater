5bed5bf635c4f58fa2aa47170b67a4c24cb21b19
ihaveamac <ihaveamac@icloud.com>
2015-11-27 12:54:30 -0800

    add safeguard in case file goes missing

--- site-2.x/updatestate.php
+++ site-2.x/updatestate.php
@@ -6,7 +6,9 @@ if ($versionh_local == "notready") {
 	$result = "DOWNLOADING";
 } else {
 	$versionh_server = trim(file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/source/version.h"));
-	if (strcmp($versionh_server, $versionh_local) !== 0) {
+	if ($versionh_server == "Not Found") {
+		$result = "READY";
+	} elseif (strcmp($versionh_server, $versionh_local) !== 0) {
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate-2/version.h", "notready"); // notready = server is busy to client
 		
 		$boot3dsx = file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/launcher.zip");
