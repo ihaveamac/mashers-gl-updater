2ea9957979dc356dd9d14841e3d8d9b31ad975e2
ihaveamac <ihaveamac@icloud.com>
2015-11-27 12:58:27 -0800

    ...and automatically disable

--- site-2.x/updatestate.php
+++ site-2.x/updatestate.php
@@ -8,6 +8,7 @@ if ($versionh_local == "notready") {
 	$versionh_server = trim(file_get_contents("https://raw.githubusercontent.com/mashers/3ds_hb_menu/master/source/version.h"));
 	if ($versionh_server == "Not Found") {
 		$result = "READY";
+		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate-2/enabled", "no");
 	} elseif (strcmp($versionh_server, $versionh_local) !== 0) {
 		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/mglupdate-2/version.h", "notready"); // notready = server is busy to client
 		
