6b42f0be384c2b15355541897450c0f311f63740
ihaveamac <ihaveamac@icloud.com>
2015-11-28 06:25:31 -0800

    change $force_download default

--- site/gh-webhook-config.php
+++ site/gh-webhook-config.php
@@ -1,3 +1,3 @@
 <?php
 $secret = "super secret"; // Put in the secret you used for the GitHub Webhook
-$force_download = true; // Force downloading files
+$force_download = false; // Force downloading files, even if they're not changed
